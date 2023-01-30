fe643873e613aed5b866004793b414c1cb8ca571
RK RAJESH <117275343+rajesh662@users.noreply.github.com>
2023-01-30 21:11:47 +0530

    Add files via upload

---
+++ while.js
@@ -0,0 +1,40 @@
+// while
+// var r=1;
+// while (r<=10){
+	// document.write(r);
+	// r++;
+// }
+
+
+
+// var a =prompt("enter they number");
+
+// var b=a%10;
+// var a=(a-b)/10;
+// var c=
+// var d=c%10;
+// var e=b*10+d;
+ 
+// document.write(e);
+ 
+ // var n=(prompt("enter they number"));
+ // while(n!==0){
+	// var a=0;
+	// var parseInt n=n/10;
+	// var  b = n%10;
+	 // var a = a*10+b;
+	 // document.write(a);
+ // }
+ 
+ var num=123;
+  for(a=num;a<=num;a!==e){
+	  if(a!==e){
+  var b=a%10;
+  var a=a-b/10;
+  var c=a%10;
+  var e=b*10+c;
+  
+	 }
+	 document.write(Math.floor(e));
+  }
+  
